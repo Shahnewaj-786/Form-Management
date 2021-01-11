4ee3eb32bb7694bfaa0b42e40e09ecf5dc28c879
Shahnewaj Sheikh <63872577+Shahnewaj-786@users.noreply.github.com>
2021-01-11 07:51:28 +0600

    Make a simple Input form using HTML and CSS

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+        <div class="user-form">
+            <h2>Add Your Info!</h2>
+            <form action="">
+                <input type="text" placeholder = "Name">    
+                <input type="text" placeholder = "Email"> 
+                <input type="text" placeholder = "Phone">  
+                <input type="submit" value = "Log In">        
+            </form>
+        </div>
+
+
+</body>
+</html>
