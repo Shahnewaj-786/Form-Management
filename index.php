d44c5a5f2e35522c380e439cd2a14708eabc7ab4
Shahnewaj Sheikh <63872577+Shahnewaj-786@users.noreply.github.com>
2021-01-12 20:44:12 +0600

    Email Validation

--- index.php
+++ index.php
@@ -18,10 +18,19 @@ if (isset ($_POST['Submit'])){
     echo $Email = $_POST['Email'];
     echo $Phone = $_POST['Phone'];
 
+
+    
+
     if(empty($Name) || empty($Email) || empty($Phone)){
 
         
         $txt= "<p style='color:red;'>Please Fill up every information</p>";
+    }elseif(!filter_var($Email, FILTER_VALIDATE_EMAIL)){
+
+//we can use !filter_var as above or -> elseif(filter_var($Email, FILTER_VALIDATE_EMAIL)==false)
+
+        $txt = "<p style='color:red;'>Invalid Email Address</p>";
+
     }else{
         $txt= "<p style='color:green;'>You are all done, THANK YOU!</p>";
     }
@@ -48,7 +57,7 @@ if (isset ($_POST['Submit'])){
             <form action="" method= "POST">
                 <input name= "Name" type="text" placeholder = "Name">    
                 <input name= "Email" type="text" placeholder = "Email"> 
-                <input name= "Phone" type="text" placeholder = "Phone">  
+                <input name= "Phone" value= "+8801" type="text" placeholder = "Phone">  
                 <input name= "Submit" type="submit" value = "Log In">        
             </form>
         </div>
